68649548726eff015fd00e0ed31e821196c8093c
TsjOgawa <44085195+TsjOgawa@users.noreply.github.com>
2021-03-04 20:21:10 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+basic.forever(function () {
+	
+})
 basic.forever(function () {
 	
 })
