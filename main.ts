fb76ecd474ab206c74779aba07444356b6c1f929
TsjOgawa <44085195+TsjOgawa@users.noreply.github.com>
2021-03-10 07:36:33 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,16 +18,23 @@ basic.forever(function () {
     let userID = 0
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
     } else {
-    	
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+            // 
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+            if (true) {
+            	
+            }
+        } else {
+            if (true) {
+            	
+            } else {
+            	
+            }
+        }
     }
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 300)
     radio.sendString("A" + "," + ("" + userID) + "," + ("" + input.acceleration(Dimension.X)) + "," + ("" + input.acceleration(Dimension.Y)) + "," + ("" + input.acceleration(Dimension.Z)))
     radio.sendString("N" + "," + ("" + userID) + "," + ("" + input.compassHeading()))
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 300)
 })
