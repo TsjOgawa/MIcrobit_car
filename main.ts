036dc07a9726218ac63b6971bf3a0c8403fbe8d1
TsjOgawa <44085195+TsjOgawa@users.noreply.github.com>
2021-03-11 19:36:58 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ basic.showLeds(`
     . . # . .
     `)
 Mode = 0
-let userID = 0
+let userID = 1
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
