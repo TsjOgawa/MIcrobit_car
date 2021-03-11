6d61471d6f46c45507f1ee58b822c625b59f0740
TsjOgawa <44085195+TsjOgawa@users.noreply.github.com>
2021-03-11 17:57:19 +0900

    無線を追加

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-def my_function():
-    pass
-maqueen.lt_event(maqueen.Patrol1.PATROL_RIGHT,
-    maqueen.Voltage.HIGH,
-    my_function)
-
-def my_function2():
-    pass
-maqueen.lt_event(maqueen.Patrol1.PATROL_LEFT,
-    maqueen.Voltage.HIGH,
-    my_function2)
-
 Mode = 0
 radio.set_group(1)
 radio.set_transmit_power(7)
@@ -22,8 +10,22 @@ basic.show_leds("""
     """)
 
 def on_forever():
-    userID = 0
-    maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 300)
-    radio.send_string("A" + "," + ("" + str(userID)) + "," + ("" + str(input.acceleration(Dimension.X))) + "," + ("" + str(input.acceleration(Dimension.Y))) + "," + ("" + str(input.acceleration(Dimension.Z))))
-    radio.send_string("N" + "," + ("" + str(userID)) + "," + ("" + str(input.compass_heading())))
+    if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+        maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 255)
+    elif maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    elif maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+                maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+            else:
+                maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
+radio.onReceivedString(function (receivedString) {
+    Mode = 1
+})
 let Mode = 0
+Mode = 0
+let userID = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
 basic.showLeds(`
@@ -9,34 +14,30 @@ basic.showLeds(`
     # # # # #
     `)
 basic.forever(function () {
-    let userID = 0
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-    } else {
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        }
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            }
-        } else {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
             if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-                if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-                    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-                    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-                    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-                        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-                    } else {
-                        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-                    }
-                }
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+            } else {
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
             }
         }
     }
-    radio.sendString("A" + "," + ("" + userID) + "," + ("" + input.acceleration(Dimension.X)) + "," + ("" + input.acceleration(Dimension.Y)) + "," + ("" + input.acceleration(Dimension.Z)))
-    radio.sendString("N" + "," + ("" + userID) + "," + ("" + input.compassHeading()))
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 300)
+    if (Mode == 1) {
+        radio.sendString("A" + "," + ("" + userID) + "," + ("" + input.acceleration(Dimension.X)) + "," + ("" + input.acceleration(Dimension.Y)) + "," + ("" + input.acceleration(Dimension.Z)))
+        radio.sendString("N" + "," + ("" + userID) + "," + ("" + input.compassHeading()))
+    }
 })
