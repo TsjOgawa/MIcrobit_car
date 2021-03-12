8210a748d72c1579c2b1bff203b672d488fd3919
TsjOgawa <44085195+TsjOgawa@users.noreply.github.com>
2021-03-12 12:29:02 +0900

    親機を作成

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("OK")
+    serial.writeLine("B" + "," + userID)
+})
+radio.onReceivedString(function (receivedString) {
+    if (Mode == 1) {
+        serial.writeLine(receivedString)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     if (Mode == 0) {
@@ -22,6 +27,7 @@ input.onButtonPressed(Button.B, function () {
             `)
     }
 })
+let userID = 0
 let Mode = 0
 radio.setGroup(1)
 basic.showLeds(`
@@ -32,31 +38,4 @@ basic.showLeds(`
     . . # . .
     `)
 Mode = 0
-let userID = 1
-basic.forever(function () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-        }
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            } else {
-                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-            }
-        }
-    }
-    if (Mode == 1) {
-        radio.sendString("A" + "," + ("" + userID) + "," + ("" + input.acceleration(Dimension.X)) + "," + ("" + input.acceleration(Dimension.Y)) + "," + ("" + input.acceleration(Dimension.Z)) + "," + ("" + input.compassHeading()))
-    }
-})
+userID = 1
