57ed7d744e75850706384ea98ba8a9d499dbc3b7
TsjOgawa <44085195+TsjOgawa@users.noreply.github.com>
2021-03-16 20:24:26 +0900

    座標データ

--- main.ts
+++ main.ts
@@ -31,33 +31,41 @@ basic.showLeds(`
     . # # # .
     . . # . .
     `)
+let coner = 0
 Mode = 0
 let userID = 1
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        coner = 0
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 200)
+        coner = 1
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 200)
+            coner = 3
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
         }
     } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+        coner = 4
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            coner = 5
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 200)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
             if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                coner = 6
                 maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
             } else {
-                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 200)
+                coner = 7
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
             }
         }
     }
     if (Mode == 1) {
-        radio.sendString("A" + "," + userID + "," + input.acceleration(Dimension.X) + "," + input.acceleration(Dimension.Y) + "," + input.acceleration(Dimension.Z) + "," + "")
+        radio.sendString("A" + "," + userID + "," + input.acceleration(Dimension.X) + "," + input.acceleration(Dimension.Y) + "," + input.acceleration(Dimension.Z))
         radio.sendString("N" + "," + userID + "," + input.compassHeading())
     }
 })
